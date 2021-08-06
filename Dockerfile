412748693f79fc14458f1749d1f5e586c11f7ae8
Christian Federmann <cfedermann@gmail.com>
2021-08-06 20:15:29 +0000

    Explicitly set versions of validation dependencies

--- Dockerfile
+++ Dockerfile
@@ -10,8 +10,9 @@ LABEL "homepage"="https://github.com/cfedermann/python-code-validator"
 LABEL "maintainer"="cfedermann"
 
 ENV PYLINT_THRESHOLD "9"
+ENV BLACK_LINE_MAXLEN "100"
 
-RUN pip install black mypy pylint-fail-under reorder-python-imports safety
+RUN pip install black==21.7b0 mypy==0.900 pylint==2.9.6 pylint-fail-under==0.3.0 reorder-python-imports==2.6.0 safety==1.10.3
 
 COPY entrypoint.sh /entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/sh -e
 
+pip install black==21.7b0 mypy==0.900 pylint==2.9.6 pylint-fail-under==0.3.0 reorder-python-imports==2.6.0 safety==1.10.3
 pip install -r requirements.txt
 
-black -S -l 75 --check .
-find . -name "*.py" | xargs mypy
-find . -name "*.py" | xargs pylint-fail-under --fail_under $PYLINT_THRESHOLD
-find . -name "*.py" | xargs reorder-python-imports --diff-only
+black -S -l $BLACK_LINE_MAXLEN --check .
+find . -name "*.py" -not -path "./.venv/*" | xargs mypy
+find . -name "*.py" -not -path "./.venv/*" | xargs pylint-fail-under --fail_under $PYLINT_THRESHOLD
+find . -name "*.py" -not -path "./.venv/*" | xargs reorder-python-imports --diff-only
 cat requirements.txt | safety check --stdin
